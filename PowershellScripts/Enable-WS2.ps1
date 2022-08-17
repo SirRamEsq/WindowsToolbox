bd3b3775a70dc3dc034e848dae6f4ac6340ea1f3
Ryan Lloyd <ryan.lloyd@xplm.com>
2022-08-17 09:04:59 -0400

    Add WS2 Enable

---
+++ PowershellScripts/Enable-WS2.ps1
@@ -0,0 +1,3 @@
+# See https://allthings.how/how-to-use-linux-terminal-in-windows-10/
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
